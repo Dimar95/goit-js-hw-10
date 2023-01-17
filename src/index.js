8aecca69bccbda9049cb9dd6bcf20b41abbba8d8
Kornilov <110562214+Dimar95@users.noreply.github.com>
2023-01-17 18:10:39 +0200

    2

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ Notify.init({
   });
 
 const refs = {
-    ulRef: document.querySelector(".country-list"),
-    divRef: document.querySelector(".country-info")
+    countryList: document.querySelector(".country-list"),
+    countryInfo: document.querySelector(".country-info")
 }
 
 
@@ -22,27 +22,48 @@ inputRef.addEventListener('input', onSearch)
 function onSearch(e) {
     let inputFilter = e.currentTarget.value.trim();
     if (inputFilter === '') {
+    refs.countryList.innerHTML = '';
         return
     }
     console.log(e.currentTarget.value);
-    onFilterCountries(fetchCountries(inputFilter))
+    fetchCountries(inputFilter).then(onFilterCountries);
+    // onFilterCountries(arrayCountries)
+    
 
 }
 
 function onFilterCountries(arrayCountries) {
+    console.log(arrayCountries)
+    refs.countryList.innerHTML = '';
+    
     if (arrayCountries.length > 10) {
         Notify.info(`❌ Too many matches found. Please enter a more specific name.`)
     } else if (arrayCountries.length > 2 && arrayCountries.length <= 10) {
-        onMurkupCountries(arrayCountries)
+        const array = arrayCountries.forEach((objectCountries) => {
+            onMurkupCountries(objectCountries)
+        })
     } else{
-        onMurkupOneCountrie(countrie)
+        onMurkupOneCountrie(arrayCountries)
     }
 }
 
-function onMurkupCountries(countries) {
+function onMurkupCountries({flags, name}) {
+
+    refs.countryList.insertAdjacentHTML('beforeend', `<li class="country-li"><img src="${flags.svg}" alt="${name.official}" width="60" height="40"><span class="country-name">${name.official}</span></li>`)
+}
+
+function onMurkupOneCountrie(arrayCountries) {
+    const countrie = arrayCountries.map(oneCountrie => oneCountrie);
+    console.log(countrie)
+
+    // console.log(flags, name, capital, population, languages)
     
 }
 
-function onMurkupOneCountrie(countrie) {
-    
-}
+
+{/* <p><img src="${flags.svg}" alt="${name.official}">${name.official}</p>
+<ul>
+  <li><b>Capital:</b><span>${capital}</span></li>
+  <li><b>Population:</b><span>${population}</span></li>
+  <li><b>Languages:</b><span>${languages}</span></li>
+</ul> */}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,8 @@
 
 export function fetchCountries(inputFilter) {
-
     const URL_COUNTRIES = 'https://restcountries.com/v3.1/name/'
+    return fetch(`${URL_COUNTRIES}${inputFilter}?fields=name,capital,population,flags,languages`).then(response => response.json()).then(data => data)
 
-    console.log(`${URL_COUNTRIES}${inputFilter}?name.official,capital,population,flags.svg,languages`);
-
-    return fetch(`${URL_COUNTRIES}${inputFilter}?fields=name.official,capital,population,flags,languages`).then(response => response.json()).then(countries => console.log(countries))
-
-    
 //    peru   
 
 }
