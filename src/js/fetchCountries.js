1f9b40d58d0b735c85163a20e66fffc778b95c92
Kornilov <110562214+Dimar95@users.noreply.github.com>
2023-01-18 16:25:57 +0200

    fix 404

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,15 @@
 
 export function fetchCountries(inputFilter) {
     const URL_COUNTRIES = 'https://restcountries.com/v3.1/name/'
-    return fetch(`${URL_COUNTRIES}${inputFilter}?fields=name,capital,population,flags,languages`).then(response => response.json()).then(data => data)
+    return fetch(`${URL_COUNTRIES}${inputFilter}?fields=name,capital,population,flags,languages`).then(response => {
+    if (response.status === 404) {
+        return Promise.reject(new Error());
+      }
+      return response.json();
+    }
+    ).then(data => data)
 
 //    peru   
 
 }
+
